47f7bfd9b4f9d71061138f0e3f840a8ce6215d6c
DineshKumar656 <dineshkumar53492@gmail.com>
2025-06-23 20:50:46 +0530

    Update Certifications.js

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -1,1 +1,12 @@
+// e.g. in src/App.js
+import Certifications from './components/Certifications';
+
+function App() {
+  return (
+    <div className="App">
+      {/* ... other sections */}
+      <Certifications />
+    </div>
+  );
+}
 
