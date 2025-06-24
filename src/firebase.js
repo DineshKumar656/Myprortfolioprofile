b55f0e7aa15f4ad26f02f74afd9ddf5422f3a545
DineshKumar656 <dineshkumar53492@gmail.com>
2025-06-24 10:48:25 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,9 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db };
 
 const firebaseConfig = {
   apiKey: "AIzaSyBsd9Vra4-MboSWLElPzcrQSNo5CTt55Es",
