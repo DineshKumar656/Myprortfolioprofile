57ae8d4af6b624a2a5acb707ff98c31066551849
DineshKumar656 <dineshkumar53492@gmail.com>
2025-06-24 10:25:38 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,14 @@ const firebaseConfig = {
   apiKey: "AIzaSyBsd9Vra4-MboSWLElPzcrQSNo5CTt55Es",
   authDomain: "dinesh-portfolio-93b90.firebaseapp.com",
   projectId: "dinesh-portfolio-93b90",
-  storageBucket: "dinesh-portfolio-93b90.appspot.com", // corrected
+  storageBucket: "dinesh-portfolio-93b90.appspot.com",
   messagingSenderId: "714521333506",
   appId: "1:714521333506:web:e5af0a19c1efdddabbfc1b",
   measurementId: "G-DQM277S2WZ"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export Firestore instance
 export const db = getFirestore(app);
